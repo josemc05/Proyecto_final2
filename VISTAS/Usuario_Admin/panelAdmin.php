ea14a15a5d7c97fd38b6528091c9823e9fbe6bba
LilianaPerezM <66573950+LilianaPerezM@users.noreply.github.com>
2021-12-12 11:15:14 -0500

    Update panelAdmin.php

--- VISTAS/Usuario_Admin/panelAdmin.php
+++ VISTAS/Usuario_Admin/panelAdmin.php
@@ -1,4 +1,11 @@
 <?php
+    include("../PHP/verificar.php");
+    include("../PHP/consultaUser.php");
+
+    if($datoUser->tipo_user!=1)
+    {
+        include("../PHP/salir.php");
+    }
 /*
 include("../../PHP/verificar.php");
 include("../../PHP/consultarUser.php");
@@ -115,6 +122,8 @@ if($datoUsuario->nivel!=1){
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                
+
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Jose Cuevas</span>
                                 <img class="img-profile rounded-circle"
                                     src="../../img/user.jpg">
